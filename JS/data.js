ecc314cf6fddbaf367db054eba6a3040c316a348
iamshihab2020 <meet2shihab18@gmail.com>
2024-02-25 04:01:53 +0600

    Data Added

---
+++ JS/data.js
@@ -0,0 +1,19 @@
+const data = {
+    "locations": ["Khulna", "Dhaka", "Chittagong", "Barisal"],
+    "boardingPoints": {
+        "Khulna": ["Sonadanga", "Fultola", "Notun Rasta"],
+        "Barishal": ["Nathullahbad", "Amtola", "Rupatoly"],
+        "Dhaka": ["Sayedabad", "Jatrabari", "Jamuna Future Park", "Gulisthan"],
+        "Chittagong": ["Bahdderhat", "New Market", "Chawkbazar"]
+    },
+    "droppingPoints": {
+        "Khulna": ["Sonadanga", "Fultola", "Notun Rasta"],
+        "Barishal": ["Nathullahbad", "Amtola", "Rupatoly"],
+        "Dhaka": ["Sayedabad", "Jatrabari", "Jamuna Future Park", "Gulisthan"],
+        "Chittagong": ["Bahdderhat", "New Market", "Chawkbazar"]
+    },
+    "classes": ["AC", "Non AC"]
+};
+
+module.exports = data;
+
